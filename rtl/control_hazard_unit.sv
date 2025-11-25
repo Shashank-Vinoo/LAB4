9e1e454b290a37f7e85c874d488507f1c37c649d
Rishabh <rishabh.rstgi06@gmail.com>
2025-11-25 18:59:39 +0000

    Control hazard unit implemented

--- rtl/control_hazard_unit.sv
+++ rtl/control_hazard_unit.sv
@@ -1,5 +1,21 @@
 module control_hazard_unit(
+    input logic pc_src,
+    input logic clk,
+    output logic rst_valid
+);
 
-)
+    always_ff @( posedge clk ) begin 
+        
+        if (pc_src) begin
+            rst_valid <= 1'b1;
+        end
+        else begin
+            rst_valid <= 1'b0;
+        end
+        
+    end
+
+
 
 endmodule
+
